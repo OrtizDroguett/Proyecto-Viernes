75630e6ce737afa1561e16f3944928961da47e8e
OrtizDroguett <j.ortizdroguett21@gmail.com>
2022-04-01 09:40:17 -0300

    primer commit de la rama pruebas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
         <h1 class="text-center mt-5">Repositorio:</h1>
-        <A HREF="https://github.com/OrtizDroguett/Proyecto-Viernes" class="center"> Link </A>
+        <A HREF="https://github.com/OrtizDroguett/Proyecto-Viernes" class="center">"         " Link </A>
 
     <img src="img\datos.PNG" alt="Girl in a jacket" width="900" height="800" class="center mt-5">
     <img src="img\modelo.PNG" alt="Girl in a jacket" width="900" height="800" class="center mt-5">
