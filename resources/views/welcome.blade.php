9af5d06354e97d5dec2a94ee6d4e75b20679e91c
OrtizDroguett <j.ortizdroguett21@gmail.com>
2022-04-02 16:10:44 -0300

    Se agrego un div con el estilo personalizado centrado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,11 @@
 @section('content')
 
         <h1 class="text-center mt-5">Repositorio:</h1>
-        <A HREF="https://github.com/OrtizDroguett/Proyecto-Viernes" class="center"> Link </A>
+        <A HREF="https://github.com/OrtizDroguett/Proyecto-Viernes" class=""> Link </A>
+        <div class=" center">
+            <img src="img\datos.PNG" alt="Girl in a jacket" width="900" height="800" class="">
+            <img src="img\modelo.PNG" alt="Girl in a jacket" width="900" height="800" class="">
+        </div>
 
-    <img src="img\datos.PNG" alt="Girl in a jacket" width="900" height="800" class="center mt-5">
-    <img src="img\modelo.PNG" alt="Girl in a jacket" width="900" height="800" class="center mt-5">
+    
 @endsection
